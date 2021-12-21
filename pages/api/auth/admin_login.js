703288d9fdbdc8ac6aecfe19674bd069ecac79a8
zalven88 <zalvendayao888@gmail.com>
2021-12-21 15:44:41 +0800

    adding controller

--- pages/api/auth/admin_login.js
+++ pages/api/auth/admin_login.js
@@ -1,18 +1,6 @@
 const ObjectId = require("mongodb").ObjectId;
 const dbConnect = require("../../config/dbConnect");
-
 dbConnect();
-/*
-
-import Cookies from "cookies";
-const jwt = require("jsonwebtoken");
-
-const createToken = (id) => {
-  return jwt.sign({ id, role: "admin" }, process.env.JWT_SECRET_KEY);
-};
-const token = createToken(findAdmin.id);
-const cookies = new Cookies(req, res);
-*/
 
 export const config = {
   api: {
@@ -22,28 +10,28 @@ export const config = {
   },
 };
 
-const getLogin = async (req, res) => {
+const getAdminLogin = async (req, res) => {
   try {
     return res.status(200).json({ success: true });
   } catch (err) {
     return res.status(401).json({ success: false, message: [] });
   }
 };
-const addLogin = async (req, res) => {
+const addAdminLogin = async (req, res) => {
   try {
     return res.status(200).json({ success: true });
   } catch (err) {
     return res.status(401).json({ success: false, message: [] });
   }
 };
-const updateLogin = async (req, res) => {
+const updateAdminLogin = async (req, res) => {
   try {
     return res.status(200).json({ success: true });
   } catch (err) {
     return res.status(401).json({ success: false, message: [] });
   }
 };
-const deleteLogin = async (req, res) => {
+const deleteAdminLogin = async (req, res) => {
   try {
     return res.status(200).json({ success: true });
   } catch (err) {
@@ -54,19 +42,19 @@ const deleteLogin = async (req, res) => {
 export default async function handler(req, res) {
   switch (req.method) {
     case "GET": {
-      return getLogin(req, res);
+      return getAdminLogin(req, res);
     }
 
     case "POST": {
-      return addLogin(req, res);
+      return addAdminLogin(req, res);
     }
 
     case "PUT": {
-      return updateLogin(req, res);
+      return updateAdminLogin(req, res);
     }
 
     case "DELETE": {
-      return deleteLogin(req, res);
+      return deleteAdminLogin(req, res);
     }
     default: {
       res.status(400).json({ sucess: false, message: [] });
