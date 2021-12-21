9b5f61ebcf86c702e2b335cc110d25076c88c779
zalven88 <zalvendayao888@gmail.com>
2021-12-21 14:13:53 +0800

    adding models

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,5 +1,7 @@
-const { connectToDatabase } = require("../../config/mongodb");
+const dbConnect = require("../../config/dbConnect");
 const ObjectId = require("mongodb").ObjectId;
+dbConnect();
+
 export default function handler(req, res) {
   res.status(200).json({ name: "John Doe" });
 }
