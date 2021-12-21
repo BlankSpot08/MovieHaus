24cb20a7e241c42d4b040c75d0cf0e3ed26cc9de
zalven88 <zalvendayao888@gmail.com>
2021-12-21 15:58:17 +0800

    adding header

--- pages/views/admin/Ticket.js
+++ pages/views/admin/Ticket.js
@@ -3,11 +3,21 @@ import { TextField, Button, MenuItem, Avatar } from "@mui/material";
 import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import axios from "axios";
 
 toast.configure();
 const Ticket = () => {
-  return <div></div>;
+  const router = useRouter();
+  return (
+    <div>
+      <Head>
+        <title>Ticket</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
 };
 
 export default Ticket;
--- pages/views/user/Home.js
+++ pages/views/user/Home.js
@@ -3,11 +3,21 @@ import { TextField, Button, MenuItem, Avatar } from "@mui/material";
 import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import { useRouter } from "next/router";
+import Head from "next/head";
 import axios from "axios";
 
 toast.configure();
 const Home = () => {
-  return <div></div>;
+  const router = useRouter();
+  return (
+    <div>
+      <Head>
+        <title>Home</title>
+        <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+      </Head>
+      <p>Hello world!</p>
+    </div>
+  );
 };
 
 export default Home;
