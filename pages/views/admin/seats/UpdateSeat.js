d30c4270f14ee696377d82e7e68ed469b75e5c2e
arvinchu <arvinchu31@gmail.com>
2021-12-23 03:09:46 +0800

    resolved minor part of create seat, fixed minor bugs in create seat

--- pages/views/admin/seats/UpdateSeat.js
+++ pages/views/admin/seats/UpdateSeat.js
@@ -1,5 +1,7 @@
 import React, { useEffect, useState } from "react";
-import { TextField, Button, MenuItem, Avatar } from "@mui/material";
+import { TextField, Button, MenuItem, Avatar, Autocomplete, Box } from "@mui/material";
+import EditIcon from '@mui/icons-material/Edit';
+import DeleteIcon from '@mui/icons-material/Delete';
 import { toast } from "react-toastify";
 import { useFormik } from "formik";
 import { useRouter } from "next/router";
@@ -14,10 +16,60 @@ import styles from "../../../../styles/Seat.module.scss";
 toast.configure();
 
 const UpdateSeat = () => {
+  const [getSeats, setSeats] = useState([{}])
+
+  const [getChosenSeat, setChosenSeat] = useState({})
+
+  React.useEffect(() => {
+    const url = '/api/admin/seat'
+    axios.get(url).then((res) => {
+      const data = res.data
+
+      let i
+      for (i = 0; i < data.value.length; i++) {
+        data.value[i]['label'] = data.value[i].seat_name
+      }
+
+      setSeats(data.value)
+    }).catch((err) => {
+      console.log(err)
+    })
+  }
+    , []);
+
   return (
-    <div>
-      <h1> Hello world </h1>
-    </div>
+    < div >
+      <Autocomplete
+        renderOption={(props, option) => (
+          <Box component="li" sx={{ '& > img': { mr: 2, flexShrink: 0 } }} {...props}>
+            {option.seat_name}
+          </Box>
+        )}
+
+        onChange={(event, value) => {
+          setChosenSeat(value)
+        }}
+
+        disablePortal
+        id="combo-box-demo"
+        options={getSeats}
+        sx={{ width: 300 }}
+        sx={{ m: 1, width: "50%" }}
+
+        renderInput={(params) => <TextField {...params} label="Seats" />}
+      />
+
+      <Button variant="contained"
+        startIcon={<EditIcon />}
+        sx={{ m: 1, height: 50 }}>
+        Edit
+      </Button>
+      <Button variant="outlined"
+        startIcon={<DeleteIcon />}
+        sx={{ m: 1, height: 50 }}>
+        Delete
+      </Button>
+    </div >
   );
 };
 
