5675c6be9e2708f5cc59baa3d0891d747e730d7b
zalven88 <zalvendayao888@gmail.com>
2021-12-28 19:18:14 +0800

    hotfix register auth module

--- pages/views/auth/Register.js
+++ pages/views/auth/Register.js
@@ -79,24 +79,40 @@ const Register = () => {
 
           <div className="flex justify-center items-center p-8 h-full">
             <div className="lg:w-2/5 md:w-1/2 w-2/3">
-              <form className="bg-black bg-opacity-20 p-5 rounded-lg shadow-lg min-w-full" onSubmit={formik.handleSubmit}>
-                <h1 className="text-center text-2xl mb-6 text-white font-bold font-sans">Register</h1>
+              <form
+                className="bg-black bg-opacity-20 p-5 rounded-lg shadow-lg min-w-full"
+                onSubmit={formik.handleSubmit}
+              >
+                <h1 className="text-center text-2xl mb-6 text-white font-bold font-sans">
+                  Register
+                </h1>
                 <div>
-                  <label className="text-white font-semibold block my-3 text-md " for="username">Username</label>
-                  <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                  <label
+                    className="text-white font-semibold block my-3 text-md "
+                    forhtml="username"
+                  >
+                    Username
+                  </label>
+                  <input
+                    className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
                     type="text"
                     name="username"
                     id="username"
                     placeholder="username"
-                    fullWidth
                     label="Username"
                     value={formik.values.username}
                     onChange={formik.handleChange}
-                    sx={{ my: 2 }} />
+                  />
                 </div>
                 <div>
-                  <label className="text-white font-semibold block my-3 text-md" for="username">Email</label>
-                  <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                  <label
+                    className="text-white font-semibold block my-3 text-md"
+                    forhtml="username"
+                  >
+                    Email
+                  </label>
+                  <input
+                    className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
                     type="text"
                     name="email"
                     id="email"
@@ -104,29 +120,40 @@ const Register = () => {
                     autoComplete="current-email"
                     value={formik.values.email}
                     onChange={formik.handleChange}
-                    sx={{ my: 2 }} />
+                  />
                 </div>
                 <div>
-                  <label className="text-white font-semibold block my-3 text-md" for="password">Password</label>
-                  <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                  <label
+                    className="text-white font-semibold block my-3 text-md"
+                    forhtml="password"
+                  >
+                    Password
+                  </label>
+                  <input
+                    className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
                     type="text"
                     name="password"
                     id="password"
                     placeholder="password"
-                    fullWidth
                     label="Password"
                     type="password"
                     autoComplete="current-password"
                     value={formik.values.password}
                     onChange={formik.handleChange}
-                    sx={{ my: 2 }} />
+                    sx={{ my: 2 }}
+                  />
                 </div>
                 <div>
-                  <label className="text-white font-semibold block my-3 text-md" for="confirm">Confirm password</label>
-                  <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                  <label
+                    className="text-white font-semibold block my-3 text-md"
+                    forhtml="confirm"
+                  >
+                    Confirm password
+                  </label>
+                  <input
+                    className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
                     type="text"
                     placeholder="confirm password"
-                    fullWidth
                     id="password_confirmation"
                     name="password_confirmation"
                     label="Confirm Password"
@@ -134,24 +161,38 @@ const Register = () => {
                     autoComplete="current-password"
                     value={formik.values.password_confirmation}
                     onChange={formik.handleChange}
-                    sx={{ my: 2 }} />
+                  />
                 </div>
                 <div>
-                  <label className="text-white font-semibold block my-3 text-md" for="mobile">Phone Number</label>
-                  <input className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
+                  <label
+                    className="text-white font-semibold block my-3 text-md"
+                    forhtml="mobile"
+                  >
+                    Phone Number
+                  </label>
+                  <input
+                    className="w-full bg-gray-100 px-4 py-2 rounded-lg focus:outline-none"
                     type="number"
                     placeholder="phone number"
                     id="mobile"
                     name="mobile"
                     label="mobile"
-                    defaultCountry={"ph"}
                     onChange={(e) => formik.setFieldValue("mobile", e)}
-                    sx={{ my: 2 }}
-                    fullWidth />
+                  />
                 </div>
-                <button type="submit" className="w-full mt-6 bg-pink-400 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans">Register</button>
+                <button
+                  type="submit"
+                  className="w-full mt-6 bg-pink-400 rounded-lg px-4 py-2 text-lg text-white tracking-wide font-semibold font-sans"
+                >
+                  Register
+                </button>
                 <a href="/views/auth/Login">
-                  <button type="button" className="w-full mt-16 bg-pink-100 rounded-lg px-4 py-2 text-lg text-pink-400 tracking-wide font-semibold font-sans">Login</button>
+                  <button
+                    type="button"
+                    className="w-full mt-16 bg-pink-100 rounded-lg px-4 py-2 text-lg text-pink-400 tracking-wide font-semibold font-sans"
+                  >
+                    Login
+                  </button>
                 </a>
               </form>
             </div>
