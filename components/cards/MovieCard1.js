fd65dcdf51b869da062a725b268d90c9930e5c05
zalven88 <zalvendayao888@gmail.com>
2021-12-31 02:49:17 +0800

    [hotfix] movie modal card1 fix schedule button

--- components/cards/MovieCard1.js
+++ components/cards/MovieCard1.js
@@ -35,6 +35,31 @@ const MovieCard1 = (props) => {
               alt="tailwind logo"
               className="rounded-xl  "
             />
+            <div className=" flex flex-row  pb-10 space-x-4 z-50 w-full my-10 ">
+              <a
+                className="flex items-center py-2 px-4 w-full rounded-full mx-auto text-white bg-red-500 hover:bg-red-700 "
+                onClick={scheduleClick}
+                data-unsp-sanitized="clean"
+              >
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  className="w-6 h-6"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
+                  ></path>
+                </svg>
+                <div className="text-sm text-white ml-2 select-none w-full">
+                  Schedule
+                </div>
+              </a>
+            </div>
           </div>
           <div className="w-full md:w-2/3 flex flex-col space-y-10 p-3">
             <div className="flex justify-between item-center">
@@ -87,32 +112,6 @@ const MovieCard1 = (props) => {
               </div>
             </div>
           </div>
-        </div>
-        <div className=" flex flex-row  pb-10 space-x-4 z-50 ">
-          <a
-            className="flex items-center py-2 px-4 rounded-full mx-auto text-white bg-red-500 hover:bg-red-700 "
-            onClick={scheduleClick}
-            data-unsp-sanitized="clean"
-          >
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                strokeWidth="2"
-                d="M12 8v4l3 3m6-3a9 9 0 11-18 0 9 9 0 0118 0z"
-              ></path>
-            </svg>
-            <div className="text-sm text-white ml-2 select-none ">
-              {" "}
-              Schedule
-            </div>
-          </a>
         </div>
       </div>
     </div>
